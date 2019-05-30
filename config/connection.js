09c12c3b2019ecf061e9ed1e27b90a07114c6cdc
androosk <46608870+androosk@users.noreply.github.com>
2019-05-30 10:07:07 -0400

    added jaws db stuff

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 // Connection to mySql database
 const mysql = require('mysql')
+var connection;
 
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'androosk',
-  password: '1234',
-  database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'androosk',
+    password: '1234',
+    database: 'burgers_db'
+  })
+}
 
 connection.connect()
 
